2fbd62d94c362fe09f989ce1236d24d8bbf5a1b6
mxstoto <85441122+mxstoto6@users.noreply.github.com>
2023-05-06 09:53:16 +0200

    Update complex-a-to-t.py

--- Scrpits/complex-a-to-t.py
+++ Scrpits/complex-a-to-t.py
@@ -1,18 +1,18 @@
 import math as m
-import fractions as f
+# import fractions as f
 
-# Define those yourself ;P
-reNumber = -8 # Rational part
-imNumber = 0 # Imaginary part
+def convert(reNumber, imNumber):
 
-modulus = m.sqrt((reNumber)**2+(imNumber)**2) # Find the modulus
+    modulus = m.sqrt((reNumber)**2+(imNumber)**2) # Find the modulus
 
-cosθ = reNumber/modulus # Find cos axis
-sinθ = imNumber/modulus # Find sin axis
+    cosθ = reNumber/modulus # Find cos axis
+    sinθ = imNumber/modulus # Find sin axis
 
-θ = (m.atan2(sinθ, cosθ)) # Find the θ angle
-# We could do `θ = f.Fraction(m.atan2(sinθ, cosθ)).limit_denominator()` to fraction it eventually
+    θ = (m.atan2(sinθ, cosθ)) # Find the θ angle
+    # We could do `θ = f.Fraction(m.atan2(sinθ, cosθ)).limit_denominator()` to fraction it eventually
 
-trForm = str(modulus) + "(cos(" + str(θ) + ") + i*sin(" + str(θ) + "))" # Assemble the expression as string
+    trForm = str(modulus) + "(cos(" + str(θ) + ") + i*sin(" + str(θ) + "))" # Assemble the expression as string
 
-print(trForm) # There you go!
+    return(trForm) # There you go!
+
+print(convert(-8, 0)) # Print the function
