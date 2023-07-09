5ae13ace4775c2ed7a91b0600ab56f2d04ffc46f
mxstoto6 <mxstotolatomate@gmail.com>
2023-07-09 16:39:15 +0200

    changed Theta for special character issue

--- Scrpits/complex-a-to-t.py
+++ Scrpits/complex-a-to-t.py
@@ -5,13 +5,13 @@ def convert(reNumber, imNumber):
 
     modulus = m.sqrt((reNumber)**2+(imNumber)**2) # Find the modulus
 
-    cosθ = reNumber/modulus # Find cos axis
-    sinθ = imNumber/modulus # Find sin axis
+    cosTheta = reNumber/modulus # Find cos axis
+    sinTheta = imNumber/modulus # Find sin axis
 
-    θ = (m.atan2(sinθ, cosθ)) # Find the θ angle
-    # We could do `θ = f.Fraction(m.atan2(sinθ, cosθ)).limit_denominator()` to fraction it eventually
+    Theta = (m.atan2(sinTheta, cosTheta)) # Find the Theta angle
+    # We could do `Theta = f.Fraction(m.atan2(sinTheta, cosTheta)).limit_denominator()` to fraction it eventually
 
-    trForm = str(modulus) + "(cos(" + str(θ) + ") + i*sin(" + str(θ) + "))" # Assemble the expression as string
+    trForm = str(modulus) + "(cos(" + str(Theta) + ") + i*sin(" + str(Theta) + "))" # Assemble the expression as string
 
     return(trForm) # There you go!
 
