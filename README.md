84ea985a1b3b1b0abdeb3753c154e2305513d531
mxstoto6 <mxstotolatomate@gmail.com>
2023-08-29 12:06:36 +0200

    added concentricCircles.py and updated README

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ So here are some small scripts i made. They're usable, but sometimes really spec
 
 ## Scripts
 
-- #### [algebraicToTrigonometric.py](https://github.com/mxstoto6/math-scripts/blob/main/Scrpits/algebraicToTrigonometric.py)   
+- #### [algebraicToTrigonometric.py](./Scrpits/algebraicToTrigonometric.py)   
     This script converts a complex number's algebraic expression to its trigonometric form.   
     The `convert` function takes 2 arguments that define the `reNumber` and `imNumber`.
 
-- #### [coinChoice.py](https://github.com/mxstoto6/math-scripts/blob/main/Scrpits/coinChoice.py)   
+- #### [coinChoice.py](./Scrpits/coinChoice.py)   
     This scripts chooses an item from a list.    
     But the twist it that it does it by flipping a coin (represented by `random.randint(0, 1)`)    
     It divides the list in two until it only contains one element. But this means that it's not completely random: for that reason we first have to shuffle the list with `random.shuffle(items)` but it kind of ruins the point of only doing it with a coin toss.
+
+- ### [concentricCircles.py](./Scrpits/concentricCircles.py)
+    This script asks for user input and calculates the area of concentric circles.
+    The radiuses can either have the same distance between them, or be specified individually.
+    The user is warned if the values provided aren't sorted correctly.
---
+++ Scrpits/concentricCircles.py
@@ -0,0 +1,42 @@
+import math as m
+
+def calc_area(radius):
+    return m.pi * radius**2
+
+def main():
+    rep = input("Is there the same distance between the circles? (yes/no) ").lower()
+    
+    if rep == "yes":
+        rmax = float(input("What is the radius of the target in cm? "))
+        dist = float(input("What is the distance in cm between 2 circles? "))
+        
+        radi = []
+        areas = []
+        
+        while rmax > dist:
+            radi.append(rmax)
+            rmax -= dist
+        
+        for i in range(len(radi)):
+            areas.append(calc_area(radi[i]) - calc_area(radi[i] - dist))
+            print(f"The area of disk {i + 1} is {areas[i]:.2f} cm²")
+
+        last_circle_area = calc_area(radi[-1] - dist)
+        print(f"The area of the central circle is {last_circle_area:.2f} cm²")
+
+    elif rep == "no":
+        nbcrcl = int(input("How many circles are there? "))
+        print("Please give the raduses in descending order:")
+        radi = [float(input(f"What is the radius of circle {i + 1}? ")) for i in range(nbcrcl)]
+
+        # Check for errors in the radiuses
+        if radi != sorted(radi, reverse=True):
+            print("Warning: Radiuses should be provided in descending order. Calculation result may be inaccurate.")
+            return
+
+        areas = [calc_area(radi[i]) - calc_area(radi[i + 1]) if i < nbcrcl - 1 else calc_area(radi[i]) for i in range(nbcrcl)]
+        
+        for i in range(nbcrcl):
+            print(f"The area of disk {i + 1} is {areas[i]:.2f} cm²")
+
+main()
